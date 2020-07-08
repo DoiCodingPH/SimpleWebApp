8f9cd3bc9b87fdf3b6c98de2dc20da1a8c063ee5
Aljon Alacapa <nojlaalacapa@gmail.com>
2020-07-09 03:02:25 +0800

    Add project files.

---
+++ SimpleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //localhost:54329/
+            //localhost:54329/Home/Index
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            //localhost:54329/Home/Post
+            return View();
+        }
+    }
+}
